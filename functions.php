c8582b276154e46f057898423c9bdd22b03e73e5
Bradley Mubenga <bradleymubenga@Bradleys-MacBook-Pro.local>
2023-01-09 19:02:58 +0200

    Unsaved Commitss

--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 function reachbeyond_register_styles() {
     $version = wp_get_theme()->get( 'Version' );
-
+    //Normalise.css
+    wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/sass/normalize.css", array(), $version, 'all');
+    //Brand Styles
     wp_enqueue_style('home_style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
-    wp_enqueue_style('reachbeyond_coming_soon', get_template_directory_uri() . "/assets/sass/temp.css", array(), $version, 'all');
+    //Home Page Styles
+    wp_enqueue_style('index_page', get_template_directory_uri() . "/assets/sass/index.css", array(), $version, 'all');
 }
 
 add_action('wp_enqueue_scripts', 'reachbeyond_register_styles');
--- index.php
+++ index.php
@@ -5,20 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Best Digital Marketing Agency in Australia | Reach Beyond Digital</title>
-
     <?php
         wp_head(); 
     ?>
 </head>
 <body>
-    <section class="temp_section">
-        <div class="temp_logo">
-            <img src="/wp-content/themes/reachbeyond-wp-template/assets/images/logo.png" alt="Reach Beyond Logo" width="250px"/>
-        </div>
-        <div class="temp_text">
-            <h1>Website Coming Soon!</h1>
-            <p>Hi there! We are currently updating our website. For any inquirie please contact <a href="mailto:bradley@reachbeyond.com">Web Developer</a>.</p>
-        </div>
-    </section>
+    <header id="header">
+        <a>
+            <image src="/wp-content/themes/reachbeyond-wp-template/assets/images/logo.png" alt="Reach Beyond Logo"/>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="/">Who We Are</a></li>
+                <li><a href="/">What We Do</a></li>
+                <li><a href="/">Case Studies</a></li>
+                <li><a href="/"><button>Contact Us</button></a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
