34ddbd11c4421a8c3ddc2ee3f1cc8251ca0e70c3
Bradley Mubenga <bradleymubenga@Bradleys-MacBook-Pro.local>
2023-01-19 18:35:36 +0200

    Responsive Header and Text Complete

--- functions.php
+++ functions.php
@@ -13,13 +13,12 @@ add_action('after_setup_theme', 'reachbeyond_theme_support');
 //Function & action to register style sheets
 function reachbeyond_register_styles() {
     $version = wp_get_theme()->get( 'Version' );
-    //Normalise.css
-    wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/css/normalize.css", array(), $version, 'all');
-    //Brand Styles
-    wp_enqueue_style('home_style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
-    //Header Styles
+    //CASSCADE FIRST
     wp_enqueue_style('header_style', get_template_directory_uri() . "/assets/css/header.css", array(), $version, 'all');
-    //Responsive Styles
+    wp_enqueue_style('home_page_style', get_template_directory_uri() . "/assets/css/home.css", array(), $version, 'all');
+    //CASSCADE LAST
+    wp_enqueue_style('normalize', get_template_directory_uri() . "/assets/css/normalize.css", array(), $version, 'all');
+    wp_enqueue_style('home_style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
     wp_enqueue_style('responsive', get_template_directory_uri() . "/assets/css/responsive.css", array(), $version, 'all');
 }
 
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
     get_header();
 ?>
-<h1>HELLO WORLD</h1>
+<section class="heroSection" id="hero">
+    <div>
+        <h1>A Digital Marketing Agency That Helps You Reach Beyond Your Goals</h1>
+        <p>We share a strong desire to connect your business to ideal prospects and deliver outstanding results.</p>
+    </div>
+    <div class="callToAction">
+        <a href="#" class="whiteCTA">Contact Us</a>
+        <span></span>
+        <a href="#" class="whiteLinedCTA">Learn More</a>
+    </div>
+</section>
 <?php
     get_footer();
 ?>
