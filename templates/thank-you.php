f2a0450b8f7e9c2d7f53d0e89375f3308afc4814
Bradley Mubenga <bradleymubenga@Bradleys-MacBook-Pro.local>
2023-01-27 13:17:14 +0200

    Website Completed 22

---
+++ templates/thank-you.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Thank You Page
+*/
+?>
+<?php
+    get_header();
+?>
+
+<section class="thankYouPage">
+    <h2>Thank you for contacting us, our team will be in touch with you shortly.</h2>
+</section>
+<?php
+    get_footer();
+?>
